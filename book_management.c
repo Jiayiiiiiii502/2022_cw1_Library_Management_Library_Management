cfa4778dfcfa3e58baa2fe924f1dcf979f9d38ca
sc20jx <sc20jx@leeds.ac.uk>
2022-03-17 12:00:17 +0800

    the original submit

---
+++ book_management.c
@@ -0,0 +1,5 @@
+//
+// Created by Jiayi on 2022/3/17.
+//
+
+#include "book_management.h"
---
+++ page.h
@@ -0,0 +1,15 @@
+//
+// Created by Jiayi on 2022/3/17.
+//
+
+#ifndef NEW_PAGE_H
+#define NEW_PAGE_H
+
+#include "book_management.h"
+
+//页面展示：
+void main_menu();  //main menu
+//void user_menu(User*);
+void librarian_menu();
+void find_book_menu();
+#endif //NEW_PAGE_H
