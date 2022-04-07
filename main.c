33c097610faca1fc897018d90f6c0ec5be21d9ce
sc20jx <sc20jx@leeds.ac.uk>
2022-04-07 09:17:28 +0800

    Finish the added function of librarian to remove/query all the users' information

--- main.c
+++ main.c
@@ -6,27 +6,29 @@
 
 int main(int argc,char*argv[]) {
     //check the command line and assgin name to library
-//    if(strcmp(argv[0],"./library")==0 && argc==3){
-//        printf("------------------------------------------------------------------------------------------------\n");
-//        printf("Welcome to the library!\n");
-//        bookfilename=(char*)malloc(sizeof(argv[1])+1);
-//        userfilename=(char*)malloc(sizeof(argv[2]+1));
-//        strcpy(bookfilename,argv[1]);
-//        strcpy(userfilename,argv[2]);
-//        main_menu();
-//        return 0;
-//    }
-//
-//    else{
-//        printf("Please enter three command lines!\n");
-//        return 0;
-//    }
-        char book[20];
-        char user[20];
-        bookfilename=(char*)malloc(sizeof(book+1));
-        userfilename=(char*)malloc(sizeof(user+1));
-        strcpy(bookfilename,"library.bin");
-        strcpy(userfilename,"users.bin");
+    //linux:
+    if(strcmp(argv[0],"./library")==0 && argc==3){
+        printf("------------------------------------------------------------------------------------------------\n");
+        printf("Welcome to the library!\n");
+        bookfilename=(char*)malloc(sizeof(argv[1])+1);
+        userfilename=(char*)malloc(sizeof(argv[2]+1));
+        strcpy(bookfilename,argv[1]);
+        strcpy(userfilename,argv[2]);
         main_menu();
         return 0;
+    }
+
+    else{
+        printf("Please enter three command lines!\n");
+        return 0;
+    }
+//windows:
+//        char book[20];
+//        char user[20];
+//        bookfilename=(char*)malloc(sizeof(book+1));
+//        userfilename=(char*)malloc(sizeof(user+1));
+//        strcpy(bookfilename,"library.bin");
+//        strcpy(userfilename,"users.bin");
+//        main_menu();
+//        return 0;
 }
